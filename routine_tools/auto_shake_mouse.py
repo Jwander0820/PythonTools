142bfa2942bf417e821ecec6d98e900e99eac600
Jwander0820 <66709644+Jwander0820@users.noreply.github.com>
2022-11-30 11:46:37 +0800

    feat: 修改防止螢幕關閉的機制、新增滑鼠座標檢測
    1.修改防止螢幕關閉的機制，改以快速點擊Capslock(切換大小寫)來防止螢幕關閉，主要原因為測試發現pyautogui移動鼠標位置還是會>導致螢幕關閉，因此改以按鍵觸發防止關閉螢幕
    2.添加滑鼠座標檢測，會先捕捉滑鼠座標位置，若經過一定時間滑鼠座標仍在相同位置時，觸發防止螢幕關閉的機制，若有移動鼠標則不會觸發，可以降低系統操作的頻率，前一版概念是定時就觸發機制
    3.因應加入了滑鼠座標檢測，檢測的秒數間隔需要修改至螢幕關閉時間的一半，因為若是在檢測完初始座標後，移動一下後因為中間有移動所以不會觸發防止關閉，再固定n分鐘就會觸發螢幕關閉了，降低檢測的間隔(=增加檢測的頻率)便可以避免該情況發生

--- routine_tools/auto_shake_mouse.py
+++ routine_tools/auto_shake_mouse.py
@@ -1,11 +1,12 @@
 from time import time, sleep
 from datetime import datetime
-from pyautogui import moveRel
+from pyautogui import moveRel, press, position
 
 
 def avoid_screen_sleep():
     while True:
-        sleep(177)  # 休眠3分鐘(-3秒)後
+        mouse_pose = position()  # 取得並暫存當前滑鼠座標
+        sleep(90)  # 休眠3分鐘(-5秒)後
         current_data_and_time = datetime.now()  # 取得當前時間
         current_time = current_data_and_time.strftime("%H:%M:%S")  # 顯示時間的格式
         current_time_int = current_data_and_time.strftime("%H%M%S")  # 用於計算的時間格式(int)
@@ -14,8 +15,15 @@ def avoid_screen_sleep():
             print("_(:з」∠)_")
             print("午休啦別再搖了")
             continue
-        moveRel(0, 1)  # 向下移動1pixel
-        print("₍₍╰(*°▽°*)╯⁾⁾ ♪")
+        if position() == mouse_pose:  # 再次取得滑鼠座標，若滑鼠座標相同，則觸發防止螢幕關閉的機制
+            # 20221130；程式移動鼠標似乎還是無法阻止螢幕關閉，可以測試不同機器看有無差異
+            # moveRel(0, 10, 1)  # 1秒內向下移動10pixel
+            # moveRel(0, -10)
+            press("capslock")  # 連續點擊兩次切換大小寫
+            press("capslock")
+            print("₍₍╰(*°▽°*)╯⁾⁾ ♪")
+        else:
+            print("_(´□`」∠)_")
 
 
 if __name__ == "__main__":
