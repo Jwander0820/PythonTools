8523021c28a952538fb8293d4d2f455353449bd8
Jwander0820 <66709644+Jwander0820@users.noreply.github.com>
2023-04-20 16:14:22 +0800

    feat: 操作檔案新增列出資料夾內檔案
    1. 操作檔案新增列出資料夾內檔案
    2. 新增使用指定key確認資料夾內是否有該資料存在，類似搜尋，但不完全是，僅是用提供的字元去比對有沒有在清單中出現

--- utils/operate_file.py
+++ utils/operate_file.py
@@ -36,6 +36,36 @@ class OperateFile:
             print(f"ERROR: {e}")
             return False
 
+    @staticmethod
+    def list_files_in_folder(folder_path: str) -> list[str]:
+        """
+        掃描指定資料夾內的檔案名稱
+        :param folder_path: 要掃描的資料夾路徑
+        :return: 檔案名稱的列表
+        """
+        try:
+            file_names = os.listdir(folder_path)
+            return file_names
+        except FileNotFoundError:
+            print(f"錯誤：找不到指定的資料夾 {folder_path}")
+            return []
+        except PermissionError:
+            print(f"錯誤：無權訪問指定的資料夾 {folder_path}")
+            return []
+
+    @staticmethod
+    def check_key_in_file_list(key: str, file_list: list[str]) -> bool:
+        """
+        檢查指定的 key 是否在檔案清單中
+        :param key: 要搜尋的關鍵字
+        :param file_list: 檔案名稱的列表
+        :return: 如果關鍵字在檔案清單中，則返回 True，否則返回 False
+        """
+        for file_name in file_list:
+            if key in file_name:
+                return True
+        return False
+
 
 if __name__ == '__main__':
     _create_folder = "./upper_folder/test_folder"
