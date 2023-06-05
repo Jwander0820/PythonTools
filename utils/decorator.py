b46ba89155ff00f00cd8fa8c2ced7acec07255ab
Jwander0820 <66709644+Jwander0820@users.noreply.github.com>
2023-06-05 16:32:08 +0800

    feat: 修改計時裝飾器方法
    1. 修改計時裝飾器用法，新增分鐘數轉換參數，可以較方便的調整輸出結果

--- utils/decorator.py
+++ utils/decorator.py
@@ -1,6 +1,7 @@
 import threading
 import time
 from functools import wraps
+from typing import Callable
 
 
 def debounce(wait):
@@ -47,37 +48,25 @@ def debounce(wait):
     return decorator
 
 
-def timer(func):
-    """
-    計時器，紀錄函數執行時間
-    :param func: 函數
-    :return:
-    """
-
-    def inner(*args, **kwargs):
-        start_time = time.time()  # 紀錄起始時間
-        try:
-            res = func(*args, **kwargs)  # 執行函數
-            end_time = time.time()  # 紀錄結束時間
-            print(f"time consume {sec2sec(start_time, end_time)} sec.")
-            return res
-        except Exception as e:
-            end_time = time.time()
-            print(f"time consume {sec2sec(start_time, end_time)} sec.")
-
-    return inner
-
-
-def sec2sec(start_time, end_time, decimal_places=2):
-    """
-    將計時器的總秒數轉換成 x 秒
-    :param start_time:計時器開始的時間
-    :param end_time:計時器結束的時間
-    :param decimal_places:要呈現多少位小數點
-    :return:
-    """
-    calculating_time_sec = round(end_time - start_time, decimal_places)
-    return calculating_time_sec
+def timer(convert_to_minutes: bool = False):
+    def inner_function(func: Callable):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            try:
+                result = func(*args, **kwargs)
+            except Exception as e:
+                print("An error occurred:", e)
+                result = None
+            finally:
+                elapsed_time = time.time() - start_time
+                if convert_to_minutes and elapsed_time > 60:
+                    minutes, seconds = divmod(elapsed_time, 60)
+                    print(f"執行時間: {int(minutes)} 分 {round(seconds, 2)} 秒")
+                else:
+                    print(f"執行時間: {round(elapsed_time, 2)} 秒")
+            return result
+        return wrapper
+    return inner_function
 
 
 class TestDecorator:
@@ -95,7 +84,7 @@ class TestDecorator:
             self.test_func()
             time.sleep(0.1)
 
-    @timer
+    @timer()
     def test_timer(self):
         time.sleep(1)
 
